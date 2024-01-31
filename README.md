3a723caf067c428e9ea1b482195d27d992afecfc
Andrew Theiss <andrew.theiss@gmail.com>
2024-01-31 03:44:44 -0800

    Base working example with output
    Ex log:
    27.16 C | 992.61 mb | -0.45 cm
    27.16 C | 992.68 mb | -1.04 cm
    27.14 C | 992.60 mb | -0.39 cm
    27.12 C | 992.59 mb | -0.30 cm....

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Rocketry-Barometer
+
+This code is specific to the BMP180.  
+BMP 180 has the following pins:
+VIN - 3.3v          (Pin 24 (labelled (3V or 3.3v on diagram))
+GND - Ground        (Pin 25 or -1 laballed GND)
+SCL - Clock         (Pin 19, I2C SCL)
+SDA - Serial Data   (Pin 18, I2C Data)
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,68 @@
-#include <Arduino.h>
+/*
+ *  BMP180    Teensy 4.0
+ *
+ *  For Wire,
+ *  SDA       18
+ *  SCL       19
+ *
+ *  For Wire1,
+ *  SDA       38
+ *  SCL       37
+ *
+ *
+ *  For Wire2,
+ *  SDA       4
+ *  SCL       3
+ *
+ *  For WireX,
+ *  You can see interfaces on the pinouts page: https://www.pjrc.com/teensy/pinout.html*
+ *
+ *
+ */
+#include <Teensy_BMP180.h>
+double baseline;
+double bmpValues[2]; //0: Temperature, 1:Pressure
+Teensy_BMP180 bmp180(&Wire);
 
-// put function declarations here:
-int myFunction(int, int);
+boolean getPressure();
+//Teensy_BMP180 bmp180(&Wire1); //If BMP180 is connected to Wire1 bus
+//Teensy_BMP180 bmp180(&Wire2); //If BMP180 is connected to Wire2 bus
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  bmp180.begin();
+  if(getPressure()){
+    baseline=bmpValues[1];
+  }
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(getPressure()){
+    double altVal=bmp180.altitude(bmpValues[1],baseline);
+    Serial.print(bmpValues[0]);
+    Serial.print(" C | ");
+    Serial.print(bmpValues[1]);
+    Serial.print(" mb | ");
+    Serial.print(altVal);
+    Serial.println(" cm");
+  }
+  delay(100);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+boolean getPressure()
+{
+  if (bmp180.startTemperature() != 0)
+  {
+    if (bmp180.getTemperature(bmpValues[0]) != 0)
+    {
+      if (bmp180.startPressure() != 0)
+      {
+        if (bmp180.getPressure(bmpValues[1],bmpValues[0]) != 0)
+        {
+          return(1);
+        }
+      }
+    }
+  }
+  return(0);
 }
